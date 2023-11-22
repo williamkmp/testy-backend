e67d5727b2aedf81d11ec943251bb8f28c7c3e8d
williamkmp <williamkurnia123@gmail.com>
2023-11-23 00:18:51 +0700

    [ADD] POST /user/image
    endpoint for uploading and changing
    registered user profile picture
    
    Changes:
    -   modified:   src/main/java/com/mito/sectask/controllers/ImageController.java
        removed: POST /image

--- src/main/java/com/mito/sectask/controllers/ImageController.java
+++ src/main/java/com/mito/sectask/controllers/ImageController.java
@@ -1,8 +1,17 @@
 package com.mito.sectask.controllers;
 
+import com.mito.sectask.annotations.Authenticated;
+import com.mito.sectask.annotations.caller.Caller;
+import com.mito.sectask.entities.ImageEntity;
+import com.mito.sectask.entities.UserEntity;
+import com.mito.sectask.exceptions.httpexceptions.RequestHttpException;
+import com.mito.sectask.exceptions.httpexceptions.UnauthorizedHttpException;
+import com.mito.sectask.services.image.ImageService;
+import com.mito.sectask.services.user.UserService;
+import com.mito.sectask.values.Message;
 import java.io.IOException;
 import java.util.Optional;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,19 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import com.mito.sectask.annotations.Authenticated;
-import com.mito.sectask.dto.response.StandardResponse;
-import com.mito.sectask.dto.response.image.ImageUploadResponse;
-import com.mito.sectask.entities.ImageEntity;
-import com.mito.sectask.entities.UserEntity;
-import com.mito.sectask.exceptions.httpexceptions.RequestHttpException;
-import com.mito.sectask.exceptions.httpexceptions.UnauthorizedHttpException;
-import com.mito.sectask.services.image.ImageService;
-import com.mito.sectask.services.user.UserService;
-import com.mito.sectask.values.Message;
-import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping(path = "/image")
@@ -32,29 +29,6 @@ public class ImageController {
 
     private final ImageService imageService;
     private final UserService userService;
-
-    @PostMapping(
-        consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    @Authenticated(true)
-    @ResponseBody
-    public StandardResponse<ImageUploadResponse> upload(
-        @RequestParam("image") MultipartFile file
-    ) throws IOException {
-        Optional<ImageEntity> maybeImage = imageService.saveImage(
-            file.getBytes()
-        );
-
-        if (maybeImage.isEmpty()) {
-            throw new RequestHttpException(Message.ERROR_UPLOAD_FAILED);
-        }
-
-        ImageEntity savedImage = maybeImage.get();
-        return new StandardResponse<ImageUploadResponse>()
-            .setStatus(HttpStatus.CREATED)
-            .setData(new ImageUploadResponse().setId(savedImage.getId()));
-    }
 
     @GetMapping("/{imageId}")
     @Authenticated(true)
@@ -73,7 +47,6 @@ public class ImageController {
     }
 
     @GetMapping("/user/{userId}")
-    @Authenticated(true)
     public ResponseEntity<byte[]> getUserImage(
         @PathVariable("userId") Long userId
     ) {
@@ -92,5 +65,29 @@ public class ImageController {
             .ok()
             .header("Content-Type", "image/*")
             .body(userImage.getFile());
+    }
+
+    @PostMapping(
+        path = "/user/image",
+        consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Authenticated(true)
+    public ResponseEntity<byte[]> uploadImageUser(
+        @RequestParam("image") MultipartFile file,
+        @Caller UserEntity caller
+    ) {
+        Optional<ImageEntity> maybeImage;
+        try {
+            maybeImage = imageService.saveImage(file.getBytes());
+        } catch (IOException e) {
+            maybeImage = Optional.empty();
+        }
+        if (maybeImage.isEmpty()) {
+            throw new RequestHttpException(Message.ERROR_UPLOAD_FAILED);
+        }
+        ImageEntity savedImage = maybeImage.get();
+        caller.setImage(savedImage);
+        return ResponseEntity.ok().build();
     }
 }
