ea963b6f9af366937f43af46ea1e3d3359aa458e
williamkmp <williamkurnia123@gmail.com>
2023-11-23 00:21:11 +0700

    [ADD] email field to UserUpdateProfileRequest

--- src/main/java/com/mito/sectask/dto/request/user/UserUpdateProfileRequest.java
+++ src/main/java/com/mito/sectask/dto/request/user/UserUpdateProfileRequest.java
@@ -3,6 +3,7 @@ package com.mito.sectask.dto.request.user;
 import com.google.gson.annotations.SerializedName;
 import com.mito.sectask.values.Regex;
 import com.mito.sectask.values.ValidationMessage;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -13,6 +14,17 @@ import lombok.Value;
  */
 @Value
 public class UserUpdateProfileRequest {
+
+    @NotBlank(message = ValidationMessage.REQUIRED)
+    @Size(
+        min = 0,
+        max = 100,
+        message = ValidationMessage.STRING_LENGTH + 0 + "," + 100
+    )
+    @Email(message = ValidationMessage.STRING_EMAIL)
+    @SerializedName("email")
+    private String email;
+
     @NotBlank(message = ValidationMessage.REQUIRED)
     @Pattern(
         regexp = Regex.TAGNAME,
