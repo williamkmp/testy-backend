967a99c81247f28b6053b4f06e27f9305fd9a684
williamkmp <williamkurnia123@gmail.com>
2023-11-27 20:53:12 +0700

    [ADD] RoleRepositor @Repository annotation

--- src/main/java/com/mito/sectask/repositories/RoleRepository.java
+++ src/main/java/com/mito/sectask/repositories/RoleRepository.java
@@ -2,8 +2,10 @@ package com.mito.sectask.repositories;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.mito.sectask.entities.RoleEntity;
 
+@Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
     
     /**
