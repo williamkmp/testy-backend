8fb8130e5d30890a727840e184f3ddd23cbf8001
williamkmp <williamkurnia123@gmail.com>
2023-11-28 21:02:23 +0700

    [UPD] RoleRepository.findByName to use USER_ROLE

--- src/main/java/com/mito/sectask/repositories/RoleRepository.java
+++ src/main/java/com/mito/sectask/repositories/RoleRepository.java
@@ -4,14 +4,18 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.mito.sectask.entities.RoleEntity;
+import com.mito.sectask.values.USER_ROLE;
 
 @Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
     
     /**
      * find roles by it's name
-     * @param name role name, which is unique
-     * @return optional role
+     * 
+     * @param   name {@link USER_ROLE}
+     *          role name, which is unique
+     * 
+     * @return  optional role
      */
-    public Optional<RoleEntity> findByName(String name);
+    public Optional<RoleEntity> findByName(USER_ROLE name);
 }
