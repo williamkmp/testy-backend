d899c9a500efc2656554d79846fe2018a61b1d09
williamkmp <williamkurnia123@gmail.com>
2023-11-28 21:04:27 +0700

    [ADD] Seeder error handling

--- src/main/java/com/mito/sectask/seeder/SeedRunner.java
+++ src/main/java/com/mito/sectask/seeder/SeedRunner.java
@@ -1,5 +1,8 @@
 package com.mito.sectask.seeder;
 
+import com.mito.sectask.seeder.impl.ProjectSeeder;
+import com.mito.sectask.seeder.impl.RoleSeeder;
+import com.mito.sectask.seeder.impl.UserProjectRoleSeeder;
 import com.mito.sectask.seeder.impl.UserSeeder;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,20 +16,32 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class SeedRunner implements CommandLineRunner {
 
-    private final UserSeeder userSeeder;
     private Logger log = LoggerFactory.getLogger(SeedRunner.class);
+    private final RoleSeeder roleSeeder;
+    private final UserSeeder userSeeder;
+    private final ProjectSeeder projectSeeder;
+    private final UserProjectRoleSeeder authoritySeeder;
 
     @Override
     public void run(String... args) throws Exception {
-        log.info("RUN SEEDER");
-        final List<Seeder> seeders = new ArrayList<>();
-        seeders.add(userSeeder);
+        try {
+            final List<Seeder> seeders = new ArrayList<>();
+            log.info("RUN SEEDER");
 
-        for (Seeder seeder : seeders) {
-            String seederName = seeder.getClass().getName();
-            log.info(String.format("    [%s]: Started", seederName));
-            seeder.seed();
-            log.info(String.format("    [%s]: Completed", seederName));
+            // Registering seeder
+            seeders.add(roleSeeder);
+            seeders.add(userSeeder);
+            seeders.add(projectSeeder);
+            seeders.add(authoritySeeder);
+
+            for (Seeder seeder : seeders) {
+                String seederName = seeder.getClass().getSimpleName();
+                log.info(String.format("    [%s]: Started", seederName));
+                seeder.seed();
+                log.info(String.format("    [%s]: Completed", seederName));
+            }
+        } catch (Exception e) {
+            log.error("Error when seeding", e);
         }
     }
 }
