c9a4dce97ded8ac6aa8b5c3a40d5ee56364f373f
williamkmp <williamkurnia123@gmail.com>
2023-11-30 00:05:06 +0700

    [ADD] Project startDate, endDate, and images

--- src/main/java/com/mito/sectask/entities/ProjectEntity.java
+++ src/main/java/com/mito/sectask/entities/ProjectEntity.java
@@ -6,10 +6,13 @@ import org.hibernate.annotations.CreationTimestamp;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -32,9 +35,23 @@ public class ProjectEntity {
     @Column(name = "description", nullable = true)
     private String description;
 
+    @Column(name = "start_date", nullable = true)
+    private Date startDate;
+
+    @Column(name = "end_date", nullable = true)
+    private Date endDate;
+
     @CreationTimestamp
     @Column(name = "created_at", nullable = false)
     private Date createdAt;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "background_image_id", referencedColumnName = "id", nullable = true)
+    private ImageEntity backgroundImage;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_image_id", referencedColumnName = "id", nullable = true)
+    private ImageEntity profileImage;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "project")
     private Set<UserProjectRoleEntity> authoritySet;
