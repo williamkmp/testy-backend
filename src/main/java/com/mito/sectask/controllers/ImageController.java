1ffdbce92cf1ad96bc0c664f85df0b120ebfe91e
williamkmp <williamkurnia123@gmail.com>
2023-12-04 23:49:25 +0700

    Del: unused images routes and services

--- src/main/java/com/mito/sectask/controllers/ImageController.java
+++ src/main/java/com/mito/sectask/controllers/ImageController.java
@@ -1,27 +1,16 @@
 package com.mito.sectask.controllers;
 
-import com.mito.sectask.annotations.Authenticated;
-import com.mito.sectask.annotations.caller.Caller;
-import com.mito.sectask.dto.response.StandardResponse;
-import com.mito.sectask.dto.response.image.ImageUploadResponse;
+import java.util.Optional;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import com.mito.sectask.entities.Image;
-import com.mito.sectask.entities.User;
 import com.mito.sectask.exceptions.httpexceptions.RequestHttpException;
 import com.mito.sectask.services.image.ImageService;
 import com.mito.sectask.values.MESSAGES;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping(path = "/image")
@@ -43,99 +32,5 @@ public class ImageController {
             .ok()
             .header("Content-Type", "image/*")
             .body(image.getFile());
-    }
-
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<byte[]> getUserImage(
-        @PathVariable("userId") Long userId
-    ) {
-        Image userProfilePicture = imageService
-            .findUserProfilePicture(userId)
-            .orElseThrow(() ->
-                new RequestHttpException(MESSAGES.UPLOAD_FAIL)
-            );
-
-        return ResponseEntity
-            .ok()
-            .header("Content-Type", "image/*")
-            .body(userProfilePicture.getFile());
-    }
-
-    @PostMapping(
-        path = "/user",
-        consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    @Authenticated(true)
-    public StandardResponse<ImageUploadResponse> uploadImageUser(
-        @RequestParam("image") MultipartFile file,
-        @Caller User caller
-    ) {
-        try {
-            Image savedImage = imageService
-                .saveUserImage(caller.getId(), file.getBytes())
-                .orElseThrow(Exception::new);
-
-            return new StandardResponse<ImageUploadResponse>()
-                .setStatus(HttpStatus.CREATED)
-                .setData(new ImageUploadResponse().setId(savedImage.getId()));
-        } catch (Exception e) {
-            throw new RequestHttpException(MESSAGES.UPLOAD_FAIL);
-        }
-    }
-
-    @DeleteMapping(path = "/user")
-    @Authenticated(true)
-    public ResponseEntity<Object> deleteImageUser(@Caller User caller) {
-        imageService.deleteUserProfilePicture(caller.getId());
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/project/profile/{projectId}")
-    public ResponseEntity<byte[]> getProjectProfile(
-        @PathVariable("projectId") Long projectId
-    ) {
-        Image picture = imageService
-            .getProjectPicture(projectId)
-            .orElseThrow(() ->
-                new RequestHttpException(MESSAGES.UPLOAD_FAIL)
-            );
-
-        return ResponseEntity
-            .ok()
-            .header("Content-Type", "image/*")
-            .body(picture.getFile());
-    }
-
-    @PostMapping(
-        path = "/project/{projectId}",
-        consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    @Authenticated(true)
-    public StandardResponse<ImageUploadResponse> uploadProjectProfile(
-        @PathVariable("projectId") Long projectId,
-        @RequestParam("image") MultipartFile file
-    ) {
-        try {
-            Image savedImage = imageService
-                .saveUserImage(projectId, file.getBytes())
-                .orElseThrow(Exception::new);
-
-            return new StandardResponse<ImageUploadResponse>()
-                .setStatus(HttpStatus.CREATED)
-                .setData(new ImageUploadResponse().setId(savedImage.getId()));
-        } catch (Exception e) {
-            throw new RequestHttpException(MESSAGES.UPLOAD_FAIL);
-        }
-    }
-
-    @DeleteMapping(path = "/project/{projectId}")
-    @Authenticated(true)
-    public ResponseEntity<Object> deleteProjectPicture(
-        @PathVariable("projectId") Long projectId
-    ) {
-        imageService.deleteProjectPicture(projectId);
-        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/mito/sectask/services/image/impl/ImageServiceImpl.java
+++ src/main/java/com/mito/sectask/services/image/impl/ImageServiceImpl.java
@@ -1,28 +1,21 @@
 package com.mito.sectask.services.image.impl;
 
-import com.mito.sectask.entities.Image;
-import com.mito.sectask.entities.Project;
-import com.mito.sectask.entities.User;
-import com.mito.sectask.repositories.ImageRepository;
-import com.mito.sectask.repositories.ProjectRepository;
-import com.mito.sectask.repositories.UserRepository;
-import com.mito.sectask.services.image.ImageService;
-import jakarta.transaction.Transactional;
 import java.util.Date;
 import java.util.Optional;
-import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import com.mito.sectask.entities.Image;
+import com.mito.sectask.repositories.ImageRepository;
+import com.mito.sectask.services.image.ImageService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
 
-    private Logger log = LoggerFactory.getLogger(ImageServiceImpl.class);
     private final ImageRepository imageRepository;
-    private final UserRepository userRepository;
-    private final ProjectRepository projectRepository;
 
     @Override
     public Optional<Image> findById(Long id) {
@@ -44,130 +37,5 @@ public class ImageServiceImpl implements ImageService {
         }
 
         return Optional.of(newImage);
-    }
-
-    @Override
-    @Transactional
-    public Optional<Image> saveUserImage(
-        Long userId,
-        byte[] imageBinary
-    ) {
-        Optional<User> maybeUser = userRepository.findById(userId);
-        if (maybeUser.isEmpty()) {
-            return Optional.empty();
-        }
-
-        User user = maybeUser.get();
-        Image userProfilePicture = user.getImage();
-        if (userProfilePicture != null) {
-            imageRepository.delete(userProfilePicture);
-        }
-
-        Optional<Image> maybeImage = saveImage(imageBinary);
-        if (maybeImage.isEmpty()) {
-            return Optional.empty();
-        }
-        Image newProfilePicture = maybeImage.get();
-        user.setImage(newProfilePicture);
-        userRepository.save(user);
-        return Optional.of(newProfilePicture);
-    }
-
-    @Override
-    @Transactional
-    public Optional<Image> findUserProfilePicture(Long userId) {
-        Optional<User> maybeUser = userRepository.findById(userId);
-        if (maybeUser.isEmpty()) {
-            return Optional.empty();
-        }
-        Image userProfilePicture = maybeUser.get().getImage();
-        if (userProfilePicture != null) userProfilePicture.getFile();
-        return Optional.ofNullable(userProfilePicture);
-    }
-
-    @Override
-    @Transactional
-    public Optional<Image> deleteUserProfilePicture(Long userId) {
-        Optional<User> maybeUser = userRepository.findById(userId);
-        if (maybeUser.isEmpty()) {
-            return Optional.empty();
-        }
-
-        User user = maybeUser.get();
-        Image userProfilePicture = user.getImage();
-        user.setImage(null);
-        userRepository.save(user);
-
-        if (userProfilePicture != null) {
-            Long imageId = userProfilePicture.getId();
-            imageRepository.deleteById(imageId);
-            return Optional.ofNullable(userProfilePicture);
-        }
-
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<Image> saveProjectImage(
-        Long projectId,
-        byte[] imageBinary
-    ) {
-        Optional<Project> maybeProject = projectRepository.findById(
-            projectId
-        );
-        if (maybeProject.isEmpty()) {
-            return Optional.empty();
-        }
-
-        Project project = maybeProject.get();
-        Image projectPicture = project.getProfileImage();
-        if (projectPicture != null) {
-            imageRepository.delete(projectPicture);
-        }
-
-        Optional<Image> maybeImage = saveImage(imageBinary);
-        if (maybeImage.isEmpty()) {
-            return Optional.empty();
-        }
-        Image newPicture = maybeImage.get();
-        project.setProfileImage(newPicture);
-        projectRepository.save(project);
-        return Optional.of(newPicture);
-    }
-
-    @Override
-    public Optional<Image> getProjectPicture(Long projectId) {
-        Optional<Project> maybeProject = projectRepository.findById(
-            projectId
-        );
-        if (maybeProject.isEmpty()) {
-            return Optional.empty();
-        }
-        Image picture = maybeProject.get().getProfileImage();
-        if (picture != null) picture.getFile();
-        return Optional.ofNullable(picture);
-    }
-
-    @Override
-    public Optional<Image> deleteProjectPicture(Long projectId) {
-        Optional<Project> maybeProject = projectRepository.findById(
-            projectId
-        );
-        if (maybeProject.isEmpty()) {
-            return Optional.empty();
-        }
-
-        Project project = maybeProject.get();
-        Image picture = project.getProfileImage();
-        project.setProfileImage(null);
-        projectRepository.save(project);
-
-        if (picture != null) {
-            Long imageId = picture.getId();
-            imageRepository.deleteById(imageId);
-            return Optional.ofNullable(picture);
-        }
-
-        return Optional.empty();
     }
 }
