a046503f69be2c2fc8757be5dfef60a60648f006
williamkmp <williamkurnia123@gmail.com>
2023-12-05 13:51:38 +0700

    Ref: remove unused constructor

--- src/main/java/com/mito/sectask/exceptions/httpexceptions/UnauthorizedHttpException.java
+++ src/main/java/com/mito/sectask/exceptions/httpexceptions/UnauthorizedHttpException.java
@@ -4,12 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpStatusCodeException;
 import com.mito.sectask.values.MESSAGES;
 
-public class UnauthorizedHttpException extends HttpStatusCodeException {
+public final class UnauthorizedHttpException extends HttpStatusCodeException {
     public UnauthorizedHttpException() {
         super(HttpStatus.UNAUTHORIZED, MESSAGES.ERROR_UNAUTHORIZED);
-    }
-
-    public UnauthorizedHttpException(String message) {
-        super(HttpStatus.UNAUTHORIZED, message);
     }
 }
