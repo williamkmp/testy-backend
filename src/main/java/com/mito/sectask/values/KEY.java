d55fedbb343157749af5fe31b49bf2077b2572ec
williamkmp <williamkurnia123@gmail.com>
2023-12-05 14:06:31 +0700

    Add: root error message key

--- src/main/java/com/mito/sectask/values/KEY.java
+++ src/main/java/com/mito/sectask/values/KEY.java
@@ -6,5 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class KEY {
 
-    public static final String REQUEST_CALLER_INFO = "REQUEST_CALLER_INFORMATION";
+    public static final String REQUEST_CALLER_INFO = "REQUEST_CALLER_INFORMATION";    
+    public static final String RESPONSE_ROOT_FORM_ERROR = "@root";
 }
