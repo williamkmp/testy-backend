e507ba82bf883bf5a639d7647a1f8c63def2f084
williamkmp <williamkurnia123@gmail.com>
2023-12-05 13:55:22 +0700

    Add: resource not found bad request alias

---
+++ src/main/java/com/mito/sectask/exceptions/httpexceptions/ResourceNotFoundHttpException.java
@@ -0,0 +1,12 @@
+package com.mito.sectask.exceptions.httpexceptions;
+
+import com.mito.sectask.values.MESSAGES;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpStatusCodeException;
+
+public final class ResourceNotFoundHttpException extends HttpStatusCodeException {
+
+    public ResourceNotFoundHttpException() {
+        super(HttpStatus.BAD_REQUEST, MESSAGES.ERROR_RESOURCE_NOT_FOUND);
+    }
+}
