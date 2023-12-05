b824a0d09e1a0f8ccbe4dccf12e25242cd9e585f
williamkmp <williamkurnia123@gmail.com>
2023-12-05 13:56:12 +0700

    add no arg cons

--- src/main/java/com/mito/sectask/dto/dto/TokenDto.java
+++ src/main/java/com/mito/sectask/dto/dto/TokenDto.java
@@ -1,11 +1,13 @@
 package com.mito.sectask.dto.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 @Data
+@NoArgsConstructor
 @Accessors(chain = true)
 public class TokenDto {
     private String refreshToken;
-    private String accessToken;    
+    private String accessToken;
 }
