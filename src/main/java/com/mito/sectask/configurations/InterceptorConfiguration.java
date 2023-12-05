68ee98f75c1fe37c994a5da5f2c4e95002ca5ebc
williamkmp <williamkurnia123@gmail.com>
2023-12-05 13:49:49 +0700

    Feat: requets & response logger init

--- src/main/java/com/mito/sectask/configurations/InterceptorConfiguration.java
+++ src/main/java/com/mito/sectask/configurations/InterceptorConfiguration.java
@@ -1,6 +1,7 @@
 package com.mito.sectask.configurations;
 
 import com.mito.sectask.interceptors.AuthenticationInterceptor;
+import com.mito.sectask.interceptors.LoggingInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,8 +13,12 @@ public class InterceptorConfiguration implements WebMvcConfigurer {
     @Autowired
     private AuthenticationInterceptor authenticationInterceptor;
 
+    @Autowired
+    private LoggingInterceptor loggerInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor).order(1);
+        registry.addInterceptor(loggerInterceptor).order(2);
     }
 }
---
+++ src/main/java/com/mito/sectask/interceptors/LoggingInterceptor.java
@@ -0,0 +1,33 @@
+package com.mito.sectask.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+@RequiredArgsConstructor
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Object handler
+    ) throws Exception {
+        // TODO implement request logger
+        return true;
+    }
+
+    @Override
+    public void postHandle(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Object handler,
+        ModelAndView modelAndView
+    ) throws Exception {
+        // TODO implement response logger
+    }
+}
