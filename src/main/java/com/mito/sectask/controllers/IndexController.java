a44435b2485a10379f4922a3820858bf4e255d74
williamkmp <williamkurnia123@gmail.com>
2023-12-05 13:55:52 +0700

    Feat: simplify ping route

--- src/main/java/com/mito/sectask/controllers/IndexController.java
+++ src/main/java/com/mito/sectask/controllers/IndexController.java
@@ -1,14 +1,17 @@
 package com.mito.sectask.controllers;
 
-import com.mito.sectask.dto.response.StandardResponse;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.system.JavaVersion;
+import org.springframework.core.SpringVersion;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.mito.sectask.dto.response.Response;
+import com.mito.sectask.values.MESSAGES;
 
 @RestController
 public class IndexController {
@@ -19,20 +22,17 @@ public class IndexController {
     @Value("${spring.application.version:0.0.1}")
     private String applicationVersion;
 
-    @GetMapping(
-        path = "/app/hi",
-        consumes = MediaType.ALL_VALUE,
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public StandardResponse<Map<String, Object>> sayHi() {
+    @GetMapping(path = "/app/ping", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response<Object> sayHi() {
         Map<String, Object> responseBody = new HashMap<>();
 
         responseBody.put("application", applicationName);
-        responseBody.put("version", applicationVersion);
+        responseBody.put("application-version", applicationVersion);
+        responseBody.put("java-version", JavaVersion.getJavaVersion());
+        responseBody.put("engine-version", "Spring_Boot-" + SpringVersion.getVersion());
         responseBody.put("dateTime", new Date());
 
-        return new StandardResponse<Map<String, Object>>()
-            .setStatus(HttpStatus.OK)
+        return new Response<Object>(HttpStatus.OK)
             .setData(responseBody);
     }
 }
