1b68fedff1180a408bc7649a5daea6fa57b243e8
williamkmp <williamkurnia123@gmail.com>
2023-12-05 14:05:59 +0700

    Fix: remove unused query

--- src/main/java/com/mito/sectask/repositories/UserRepository.java
+++ src/main/java/com/mito/sectask/repositories/UserRepository.java
@@ -1,12 +1,9 @@
 package com.mito.sectask.repositories;
 
-import com.mito.sectask.entities.User;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import com.mito.sectask.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -14,7 +11,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);    
     
     Optional<User> findByTagName(String tagName);
-
-    @Query("FROM UserEntity AS u WHERE u.email IN :emails")
-    List<User> findByEmailList(@Param("emails") List<String> emails);
 }
