1ae895840c0e68c8949ab7da320715cdd097786b
williamkmp <williamkurnia123@gmail.com>
2023-12-12 19:27:20 +0700

    Feat: add pages parent-children relationship

--- src/main/java/com/mito/sectask/entities/Page.java
+++ src/main/java/com/mito/sectask/entities/Page.java
@@ -1,15 +1,19 @@
 package com.mito.sectask.entities;
 
 import java.util.Date;
-import java.util.Set;
+import java.util.List;
 import org.hibernate.annotations.CreationTimestamp;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -30,10 +34,24 @@ public class Page {
     @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "image_position", nullable = false)
+    private Float imagePosition = (float) 0;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "image_id", referencedColumnName = "id", nullable = true)
+    private File image;
+
     @CreationTimestamp
     @Column(name = "created_at", nullable = false)
     private Date createdAt;
 
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Page> childrens; 
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "parent_id", nullable = true)
+    private Page parent;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "page")
-    private Set<Authority> authoritySet;
+    private List<Authority> authorities;
 }
