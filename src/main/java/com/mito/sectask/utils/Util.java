8e0dbe74d46a8a3eb7facb9454dd28bdf2871151
williamkmp <williamkurnia123@gmail.com>
2023-12-14 20:32:17 +0700

    Feat: Add Utils.String.toLong()

--- src/main/java/com/mito/sectask/utils/Util.java
+++ src/main/java/com/mito/sectask/utils/Util.java
@@ -1,5 +1,7 @@
 package com.mito.sectask.utils;
 
+import java.util.Optional;
+
 /**
  * Utilities class filled with static method to
  * perform common used stateless functionality
@@ -8,7 +10,7 @@ package com.mito.sectask.utils;
  * @author william.kmp
  */
 public class Util {
-
+    
     /**
      * method to do nothing, usually to be used inside
      * a try catch block when the exception is handled
@@ -16,8 +18,29 @@ public class Util {
      * @param   reason developer notes describing
      *          why no further action is needed
      */
-    public static void doNothing(String reason) {
+    public static void doNothing(java.lang.String reason) {
         // do nothing
         return;
+    }
+
+
+    public static class String {
+        
+        private String() {}
+
+        public static Optional<Long> toLong(java.lang.String string) {
+            Long longVal = null;
+
+            if(string == null) 
+                return Optional.empty();
+
+            try {
+                longVal = Long.valueOf(string);
+            } catch (NumberFormatException e) {
+                doNothing("longVal is alredy null");
+            }
+            return Optional.ofNullable(longVal);
+        }
+        
     }
 }
