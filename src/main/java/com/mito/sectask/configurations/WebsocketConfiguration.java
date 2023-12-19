163ddf9bfce86c1bd2440336041559cbd6a50df6
williamkmp <williamkurnia123@gmail.com>
2023-12-20 00:11:13 +0700

    Feat: add websocket integration endpoint

--- src/main/java/com/mito/sectask/configurations/WebsocketConfiguration.java
+++ src/main/java/com/mito/sectask/configurations/WebsocketConfiguration.java
@@ -1,11 +1,50 @@
 package com.mito.sectask.configurations;
 
+import com.google.gson.Gson;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.GsonMessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer{
+@RequiredArgsConstructor
+public class WebsocketConfiguration
+    implements WebSocketMessageBrokerConfigurer {
 
+    private final Gson gson;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/user", "/page");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(
+        StompEndpointRegistry stompEndpointRegistry
+    ) {
+        stompEndpointRegistry
+            .addEndpoint("/ws")
+            .setAllowedOriginPatterns("*");
+
+        stompEndpointRegistry
+            .addEndpoint("/ws")
+            .setAllowedOriginPatterns("*")
+            .withSockJS();
+    }
+
+    @Override
+    public boolean configureMessageConverters(
+        List<MessageConverter> messageConverters
+    ) {
+        GsonMessageConverter messageConverter = new GsonMessageConverter(gson);
+        messageConverters.add(messageConverter);
+        return false;
+    }
 }
