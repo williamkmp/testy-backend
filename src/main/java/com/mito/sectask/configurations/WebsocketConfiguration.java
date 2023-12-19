eb0b1fc3bd07b2b21aa5af5d4726a51eb4f048b0
williamkmp <williamkurnia123@gmail.com>
2023-12-19 19:04:31 +0700

    Init websocket integration

---
+++ src/main/java/com/mito/sectask/configurations/WebsocketConfiguration.java
@@ -0,0 +1,11 @@
+package com.mito.sectask.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer{
+
+}
