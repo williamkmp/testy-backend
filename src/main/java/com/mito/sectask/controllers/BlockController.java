7cbf30e8c9e95ac06d106797a809e2db6040cd9a
williamkmp <williamkurnia123@gmail.com>
2024-01-10 01:41:43 +0700

    init: block controller

--- src/main/java/com/mito/sectask/controllers/BlockController.java
+++ src/main/java/com/mito/sectask/controllers/BlockController.java
@@ -1,5 +1,21 @@
 package com.mito.sectask.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+import com.mito.sectask.dto.dto.MenuPreviewDto;
+import com.mito.sectask.dto.response.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequiredArgsConstructor
 public class BlockController {
+    
+    @GetMapping("/")
+    public Response<MenuPreviewDto> getMethodName(@RequestParam String param) {
+        // TODO: impelement me
+        throw new UnsupportedOperationException("endpoint not implemented yed");
+    }
     
 }
