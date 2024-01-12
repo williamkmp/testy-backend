fb9b9547561b395493a08914676f31bb6c05ac79
williamkmp <williamkurnia123@gmail.com>
2024-01-12 18:19:46 +0700

    feat: implement authority seeder

--- src/main/java/com/mito/sectask/seeder/impl/AuthoritySeeder.java
+++ src/main/java/com/mito/sectask/seeder/impl/AuthoritySeeder.java
@@ -1,26 +1,130 @@
 package com.mito.sectask.seeder.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
 import org.springframework.stereotype.Component;
+import com.mito.sectask.entities.Authority;
+import com.mito.sectask.entities.Page;
+import com.mito.sectask.entities.Role;
+import com.mito.sectask.entities.User;
 import com.mito.sectask.repositories.AuthorityRepository;
-import com.mito.sectask.repositories.PageRepository;
-import com.mito.sectask.repositories.RoleRepository;
-import com.mito.sectask.repositories.UserRepository;
 import com.mito.sectask.seeder.Seeder;
+import com.mito.sectask.services.page.PageService;
+import com.mito.sectask.services.role.RoleService;
+import com.mito.sectask.services.user.UserService;
+import com.mito.sectask.values.USER_ROLE;
+import jakarta.transaction.Transactional;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Component
 @RequiredArgsConstructor
 public class AuthoritySeeder implements Seeder {
 
-    private final RoleRepository roleRepository;
     private final AuthorityRepository authorityRepository;
-    private final PageRepository pageRepository;
-    private final UserRepository userRepository;
-
+    private final RoleService roleService;
+    private final PageService pageService;
+    private final UserService userService;
 
     @Override
+    @Transactional
     public void seed() throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'seed'");
+        ProjectConfiguration configuration1 = new ProjectConfiguration()
+            .setPageId(1L) // Testy
+            .setOwnerEmail("aisyah@email.com")
+            .addMember("william@email.com")
+            .addMember("andre@email.com");
+
+        ProjectConfiguration configuration2 = new ProjectConfiguration()
+            .setPageId(2L) //Binusmaya Website
+            .setOwnerEmail("william@email.com")
+            .addMember("calvin@email.com")
+            .addMember("stefan@email.com");
+
+        ProjectConfiguration configuration3 = new ProjectConfiguration()
+            .setPageId(3L) // Binus Mobile
+            .setOwnerEmail("william@email.com")
+            .addMember("filipus@email.com")
+            .addMember("owen@email.com");
+
+        List<Authority> authorities = Stream
+            .of(
+                construcAuthorities(configuration1),
+                construcAuthorities(configuration2),
+                construcAuthorities(configuration3)
+            )
+            .flatMap(Collection::stream)
+            .toList();
+
+        authorityRepository.saveAllAndFlush(authorities);
+    }
+
+    private List<Authority> construcAuthorities(
+        ProjectConfiguration configuration
+    ) {
+        List<Authority> authorities = new ArrayList<>();
+        final Role OWNER_ROLE = roleService.getRole(USER_ROLE.FULL_ACCESS);
+        final Role MEMBER_ROLE = roleService.getRole(USER_ROLE.COLLABORATORS);
+
+        // Getting the page
+        Long pageId = configuration.getPageId();
+        Page page = pageService
+            .findById(pageId)
+            .orElseThrow(() -> new Error("Page [" + pageId + "] not found"));
+
+        // Adding owner authoritiy
+        String ownerEmail = configuration.getOwnerEmail();
+        User owner = userService
+            .findByEmail(ownerEmail)
+            .orElseThrow(() ->
+                new Error("User with email [" + ownerEmail + "] not found")
+            );
+
+        authorities.add(
+            new Authority()
+                .setPage(page)
+                .setRole(OWNER_ROLE)
+                .setUser(owner)
+                .setIsPending(false)
+        );
+
+        // Adding members authorities
+        List<String> memberEmails = List.copyOf(configuration.getMemberEmail());
+        List<User> members = userService.findAllByEmails(memberEmails);
+        for (User member : members) {
+            authorities.add(
+                new Authority()
+                    .setPage(page)
+                    .setRole(MEMBER_ROLE)
+                    .setUser(member)
+                    .setIsPending(false)
+            );
+        }
+
+        return authorities;
+    }
+
+    /**
+     *
+     */
+    @Data
+    @NoArgsConstructor
+    @Accessors(chain = true)
+    private class ProjectConfiguration {
+
+        private Long pageId;
+        private String ownerEmail;
+        private Set<String> memberEmail = new HashSet<>();
+
+        public ProjectConfiguration addMember(String email) {
+            this.memberEmail.add(email);
+            return this;
+        }
     }
 }
