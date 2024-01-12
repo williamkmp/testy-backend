d54bb059652acee92a2fd7c1525a36339384e52b
williamkmp <williamkurnia123@gmail.com>
2024-01-12 18:16:31 +0700

    feat: add findByEmails

--- src/main/java/com/mito/sectask/repositories/UserRepository.java
+++ src/main/java/com/mito/sectask/repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package com.mito.sectask.repositories;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.mito.sectask.entities.User;
 
@@ -9,6 +12,9 @@ import com.mito.sectask.entities.User;
 public interface UserRepository extends JpaRepository<User, Long> {
     
     Optional<User> findByEmail(String email);    
+
+    @Query("SELECT u FROM User u WHERE u.email IN :emails")
+    List<User> findByEmails(@Param("emails") List<String> emails); 
     
     Optional<User> findByTagName(String tagName);
 }
