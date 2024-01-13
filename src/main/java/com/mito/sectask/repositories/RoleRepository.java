9d0836484872c23428c72e903ed8d1fd2ddbd13b
williamkmp <williamkurnia123@gmail.com>
2024-01-13 15:31:45 +0700

    feat: add findByRootPageId() change return type of findByName()

--- src/main/java/com/mito/sectask/repositories/RoleRepository.java
+++ src/main/java/com/mito/sectask/repositories/RoleRepository.java
@@ -4,6 +4,8 @@ import com.mito.sectask.entities.Role;
 import com.mito.sectask.values.USER_ROLE;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,5 +16,29 @@ public interface RoleRepository extends JpaRepository<Role, Long> {
      * @param name {@link USER_ROLE} role name, which is unique
      * @return optional role
      */
-    public Optional<Role> findByName(USER_ROLE name);
+    public Role findByName(USER_ROLE name);
+
+    /**
+     * get User role for a certain 
+     * 
+     * @param rootPageId {@link Long} must be a root page id
+     * @param userId {@link Long} user id
+     * @return {@link Optional}<{@link Role}> conatining user role for a certain page, else Optional.empty()
+     */
+    @Query("""
+            SELECT
+                r
+            FROM 
+                Role r 
+                JOIN FETCH r.authorities a
+                JOIN FETCH a.page p
+                JOIN FETCH a.user u
+            WHERE
+                p.id = :rootPageId
+                AND u.id = :userId
+            """)
+    public Optional<Role> findByRootPageId(
+        @Param("rootPageId") long rootPageId,
+        @Param("userId") long userId
+    );
 }
