14bb7e0287589dd3bdd5b5350a09f74fb17204b7
williamkmp <williamkurnia123@gmail.com>
2024-01-13 09:13:57 +0700

    feat: add blockRepository.findAllByPageId()

--- src/main/java/com/mito/sectask/repositories/BlockRepository.java
+++ src/main/java/com/mito/sectask/repositories/BlockRepository.java
@@ -1,8 +1,26 @@
 package com.mito.sectask.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.mito.sectask.entities.Block;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BlockRepository extends JpaRepository<Block, String> {}
+public interface BlockRepository extends JpaRepository<Block, String> {
+
+    @Query(
+            nativeQuery = true,
+            value ="""
+			WITH RECURSIVE cte AS
+			(
+				SELECT * FROM blocks AS b WHERE b.page_id = 1 AND b.prev_id is null
+				UNION
+				SELECT blocks.* FROM cte JOIN blocks ON cte.id = blocks.prev_id
+			)
+			SELECT * FROM cte AS blocks
+			"""
+	)
+    public List<Block> findAllByPageId(@Param("pageId") Long pageId);
+}
