8e6c5ad8f61e02f29f034aa85d2225d488989f8d
williamkmp <williamkurnia123@gmail.com>
2024-01-14 23:50:45 +0700

    ref: remove unused placehoder code

--- src/main/java/com/mito/sectask/controllers/BlockController.java
+++ src/main/java/com/mito/sectask/controllers/BlockController.java
@@ -1,19 +1,10 @@
 package com.mito.sectask.controllers;
 
-import com.mito.sectask.dto.dto.MenuPreviewDto;
-import com.mito.sectask.dto.response.Response;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class BlockController {
-
-    @GetMapping("/")
-    public Response<MenuPreviewDto> getMethodName(@RequestParam String param) {
-        // TODO: impelement me
-        throw new UnsupportedOperationException("endpoint not implemented yed");
-    }
-}
+@RequestMapping("/block")
+public class BlockController {}
