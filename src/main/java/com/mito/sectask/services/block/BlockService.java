3b39596d072a99100f543b560c01339175143104
williamkmp <williamkurnia123@gmail.com>
2024-01-14 23:50:26 +0700

    feat: add BlockService.findAllCollectionByPageId()

--- src/main/java/com/mito/sectask/services/block/BlockService.java
+++ src/main/java/com/mito/sectask/services/block/BlockService.java
@@ -1,6 +1,8 @@
 package com.mito.sectask.services.block;
 
 import com.mito.sectask.entities.Block;
+import com.mito.sectask.exceptions.exceptions.ResourceNotFoundException;
+import java.util.List;
 import java.util.Optional;
 
 public interface BlockService {
@@ -12,4 +14,13 @@ public interface BlockService {
      * @return {@link Optional}<{@link Block}> Block entity, else Option.empty()
      */
     public Optional<Block> findById(String blockId);
+
+    /**
+     * get list of COLLECTION typed block from a given page
+     *
+     * @param pageId {@link Long} page id
+     * @return {@link List}<{@link Block}> list of block with type of COLEECTION
+     * @throws {@link ResourceNotFoundException} when the given page not exist
+     */
+    public List<Block> findAllCollectionByPageId(Long pageId) throws ResourceNotFoundException;
 }
--- src/main/java/com/mito/sectask/services/block/impl/BlockServiceImpl.java
+++ src/main/java/com/mito/sectask/services/block/impl/BlockServiceImpl.java
@@ -1,9 +1,15 @@
 package com.mito.sectask.services.block.impl;
 
 import com.mito.sectask.entities.Block;
+import com.mito.sectask.entities.Page;
+import com.mito.sectask.exceptions.exceptions.ResourceNotFoundException;
 import com.mito.sectask.repositories.BlockRepository;
+import com.mito.sectask.repositories.PageRepository;
 import com.mito.sectask.services.block.BlockService;
+import com.mito.sectask.values.BLOCK_TYPE;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +18,20 @@ import org.springframework.stereotype.Service;
 public class BlockServiceImpl implements BlockService {
 
     private final BlockRepository blockRepository;
+    private final PageRepository pageRepository;
 
     @Override
     public Optional<Block> findById(String blockId) {
         if (blockId == null) return Optional.empty();
         return blockRepository.findById(blockId);
+    }
+
+    @Override
+    public List<Block> findAllCollectionByPageId(Long pageId) {
+        Page page = pageRepository.findById(pageId).orElseThrow(ResourceNotFoundException::new);
+        List<Block> blocks = page.getBlocks();
+        return blocks.stream()
+                .filter(block -> block.getBlockType().equals(BLOCK_TYPE.COLLECTION))
+                .collect(Collectors.toList());
     }
 }
