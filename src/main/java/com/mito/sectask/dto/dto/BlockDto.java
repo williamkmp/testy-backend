c8bccc747b9044edbd8be6c4b79118b64b837ea6
williamkmp <williamkurnia123@gmail.com>
2024-01-19 02:10:04 +0700

    feat: add BlockDto

---
+++ src/main/java/com/mito/sectask/dto/dto/BlockDto.java
@@ -0,0 +1,18 @@
+package com.mito.sectask.dto.dto;
+
+import com.mito.sectask.values.BLOCK_TYPE;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
+public class BlockDto {
+    private String id;
+    private BLOCK_TYPE type;
+    private String content;
+    private String iconKey;
+    private Float width;
+    private String fileId;
+}
