ff4ca74ce80ce5312bc9c0d4dafccc0fb1425edf
williamkmp <williamkurnia123@gmail.com>
2024-01-20 21:04:02 +0700

    ref: rename AnnotationResolverCOnfiguration to HttpAnnotationResolverConfiguration

--- src/main/java/com/mito/sectask/configurations/HttpAnnotationResolverConfiguration.java
+++ src/main/java/com/mito/sectask/configurations/HttpAnnotationResolverConfiguration.java
@@ -1,6 +1,7 @@
 package com.mito.sectask.configurations;
 
 import com.mito.sectask.annotations.caller.CallerResolver;
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +9,13 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AnnotationResoverConfiguration implements WebMvcConfigurer {
+public class HttpAnnotationResolverConfiguration implements WebMvcConfigurer {
 
     @Autowired
-    private CallerResolver callerAnnotationResolver;
+    private CallerResolver callerResolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(callerAnnotationResolver);
+        resolvers.add(callerResolver);
     }
 }
