ac343ce4f95f9062bea863b9b1d56caad72684c5
williamkmp <williamkurnia123@gmail.com>
2024-01-20 21:05:28 +0700

    feat: add SENDER_HEADER_KEY

--- src/main/java/com/mito/sectask/values/KEY.java
+++ src/main/java/com/mito/sectask/values/KEY.java
@@ -8,4 +8,5 @@ public class KEY {
 
     public static final String REQUEST_CALLER_INFO = "REQUEST_CALLER_INFORMATION";
     public static final String RESPONSE_ROOT_FORM_ERROR = "@root";
+    public static final String SENDER_HEADER_KEY = "userId";
 }
