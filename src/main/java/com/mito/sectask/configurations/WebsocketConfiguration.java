5d581f3ae0145c72ea35392c697d047297aac5c5
williamkmp <williamkurnia123@gmail.com>
2024-01-20 21:05:09 +0700

    feat: register @sender resolver annotation

--- src/main/java/com/mito/sectask/configurations/WebsocketConfiguration.java
+++ src/main/java/com/mito/sectask/configurations/WebsocketConfiguration.java
@@ -1,11 +1,14 @@
 package com.mito.sectask.configurations;
 
 import com.google.gson.Gson;
+import com.mito.sectask.annotations.sender.SenderResolver;
+
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.GsonMessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -17,6 +20,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
     private final Gson gson;
+    private final SenderResolver senderResolver;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -34,5 +38,11 @@ public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer 
         GsonMessageConverter messageConverter = new GsonMessageConverter(gson);
         messageConverters.add(messageConverter);
         return false;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        WebSocketMessageBrokerConfigurer.super.addArgumentResolvers(resolvers);
+        resolvers.add(senderResolver);
     }
 }
