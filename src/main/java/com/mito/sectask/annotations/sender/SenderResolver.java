a0383bdcf7f34fc94440dff0f3cec205d23c0175
williamkmp <williamkurnia123@gmail.com>
2024-01-20 21:04:34 +0700

    feat: add @Sender annotation parameter resolver

---
+++ src/main/java/com/mito/sectask/annotations/sender/SenderResolver.java
@@ -0,0 +1,35 @@
+package com.mito.sectask.annotations.sender;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+import com.mito.sectask.entities.User;
+import com.mito.sectask.repositories.UserRepository;
+import com.mito.sectask.values.KEY;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class SenderResolver implements HandlerMethodArgumentResolver {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return (parameter.hasParameterAnnotation(Sender.class)
+                && User.class.isAssignableFrom(parameter.getParameterType()));
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, Message<?> message) throws Exception {
+        SimpMessageHeaderAccessor header = SimpMessageHeaderAccessor.wrap(message);
+        Long senderId = Long.valueOf(header.getFirstNativeHeader(KEY.SENDER_HEADER_KEY));
+        return userRepository.findById(senderId).orElse(null);
+    }
+
+    
+}
