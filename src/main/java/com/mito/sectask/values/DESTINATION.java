b6f3e87b99af6e0e7f2e9ba766eb68349d7882d9
williamkmp <williamkurnia123@gmail.com>
2024-01-21 12:50:28 +0700

    feat: add stomp DESTINATION value class

---
+++ src/main/java/com/mito/sectask/values/DESTINATION.java
@@ -0,0 +1,58 @@
+package com.mito.sectask.values;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DESTINATION {
+    
+    /**
+     * STOMP destination url for /queue/user/{userId}/preview
+     * 
+     * @param userId {@link Long} user id
+     * @return {@link String} STOMP destination path
+     */
+    public static String userPreview(Long userId) {
+        return "/queue/user/" + userId.toString() + "/preview"; 
+    }
+
+    /**
+     * STOMP destination url for /queue/user/{userId}/notification
+     * 
+     * @param userId {@link Long} user id
+     * @return {@link String} STOMP destination path
+     */
+    public static String userNotification(Long userId) {
+        return "/queue/user/" + userId.toString() + "/notification"; 
+    }
+
+    /**
+     * STOMP destination url for /topic/page/{pageId}/header
+     * 
+     * @param pageId {@link Long} page id
+     * @return {@link String} STOMP destination path
+     */
+    public static String pageUpdate(Long pageId) {
+        return "/topic/page/" + pageId.toString() + "/preview"; 
+    }
+
+    /**
+     * STOMP destination url for /topic/page/{pageId}/block
+     * 
+     * @param pageId {@link Long} page id
+     * @return {@link String} STOMP destination path
+     */
+    public static String pageBlock(Long pageId) {
+        return "/topic/page/" + pageId.toString() + "/block"; 
+    }
+
+    /**
+     * STOMP destination url for /topic/page/{pageId}/collection
+     * 
+     * @param pageId {@link Long} page id
+     * @return {@link String} STOMP destination path
+     */
+    public static String pageCollection(Long pageId) {
+        return "/topic/page/" + pageId.toString() + "/collection"; 
+    }
+}
