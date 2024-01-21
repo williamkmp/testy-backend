218888b3c1fae26fbdd7fce0c2b498ec0e2fcca0
williamkmp <williamkurnia123@gmail.com>
2024-01-21 17:42:53 +0700

    feat: add PreviewMessageDto

---
+++ src/main/java/com/mito/sectask/dto/dto/PreviewMessageDto.java
@@ -0,0 +1,19 @@
+package com.mito.sectask.dto.dto;
+
+import com.mito.sectask.values.PREVIEW_ACTION;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
+public class PreviewMessageDto {
+    private PREVIEW_ACTION action;
+    
+    private String parentId;
+    private String id;
+    private String name;
+    private String iconKey;
+}
