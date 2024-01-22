1422a128e70e2c828a471b5e910c43593ecf3ac0
williamkmp <williamkurnia123@gmail.com>
2024-01-22 20:17:19 +0700

    feat: rename KEY to be more descriptive

--- src/main/java/com/mito/sectask/values/KEY.java
+++ src/main/java/com/mito/sectask/values/KEY.java
@@ -9,5 +9,6 @@ public class KEY {
     public static final String REQUEST_CALLER_INFO =
         "REQUEST_CALLER_INFORMATION";
     public static final String RESPONSE_ROOT_FORM_ERROR = "@root";
-    public static final String SENDER_HEADER_KEY = "userId";
+    public static final String SENDER_USER_ID = "userId";
+    public static final String SENDER_SESSION_ID = "sessionId";
 }
