bd6650cc244ac44203d3d8e1368304e3877588e9
williamkmp <williamkurnia123@gmail.com>
2024-01-26 16:03:24 +0700

    feat: add PageHeaderMessageDto

---
+++ src/main/java/com/mito/sectask/dto/dto/PageHeaderMessageDto.java
@@ -0,0 +1,15 @@
+package com.mito.sectask.dto.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+public class PageHeaderMessageDto {
+    private String title;
+    private String iconKey;
+    private String imageId;
+    private Float imagePosition;
+}
