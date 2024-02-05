61ed148dd526814dfcf1ab31e5ebc7a144909419
williamkmp <williamkurnia123@gmail.com>
2024-02-05 17:45:14 +0700

    feat: add PageMessagingExceptionDto

---
+++ src/main/java/com/mito/sectask/dto/dto/PageMessagingExceptionDto.java
@@ -0,0 +1,16 @@
+package com.mito.sectask.dto.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+public class PageMessagingExceptionDto {
+
+    private Integer status;
+    private String userId;
+    private String pageId;
+    private String message;
+}
